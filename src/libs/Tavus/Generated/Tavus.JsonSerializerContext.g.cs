f358e20c71e89633e70ec70cc4005f428fc88ddc
Konstantin S. <havendv@gmail.com>
2026-08-29 09:15:36 +0400

    feat: Updated OpenAPI spec (#42)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Tavus/Generated/Tavus.JsonSerializerContext.g.cs
+++ src/libs/Tavus/Generated/Tavus.JsonSerializerContext.g.cs
@@ -64,6 +64,7 @@ namespace Tavus
             typeof(global::Tavus.JsonConverters.UnixTimestampJsonConverter),
         })]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Tavus.JsonSerializerContextTypes))]
+    [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::System.Collections.Generic.List<object>), TypeInfoPropertyName = "SystemCollectionsGeneric_ObjectList")]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(global::Tavus.CreateConversationRequest))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(string))]
     [global::System.Text.Json.Serialization.JsonSerializable(typeof(bool))]
